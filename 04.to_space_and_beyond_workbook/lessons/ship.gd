8df0fcc193eb62f57935176aae07c71dd9763a21
Purple9200 <anxie8@s.sfusd.edu>
2024-03-21 11:34:02 -0700

    L5 Steering Smoothly

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -4,14 +4,16 @@ var boost_speed :=1500.0
 var max_speed := 600.0
 var velocity := Vector2(0, 0)
 var direction := Vector2(0, 0)
-
+var steering_factor :=10.0
 func _process(delta: float) -> void:
 	direction.x= Input.get_axis("move_left","move_right")
 	direction.y= Input.get_axis("move_up","move_down")
 	if Input.is_action_just_pressed("boost"):
 		max_speed = boost_speed
 		get_node("Timer").start()
-	velocity = direction * max_speed
+	var desired_velocity := max_speed*direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector*steering_factor*delta
 	position += velocity * delta
 	if velocity.length() > 0.0:
 		rotation = velocity.angle()
