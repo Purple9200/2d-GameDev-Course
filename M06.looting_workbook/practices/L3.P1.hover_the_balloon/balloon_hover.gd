78209c7634854cba96716d1a4d9e3c11a82b00b4
Purple9200 <anxie8@s.sfusd.edu>
2024-05-03 11:36:10 -0700

    M6 L6 P1

--- M06.looting_workbook/practices/L3.P1.hover_the_balloon/balloon_hover.gd
+++ M06.looting_workbook/practices/L3.P1.hover_the_balloon/balloon_hover.gd
@@ -3,6 +3,7 @@ extends Area2D
 @onready var canvas_group: CanvasGroup = $CanvasGroup
 @onready var canvas_group_has_material: bool = canvas_group.material != null
 
+
 func _ready() -> void:
 	set_outline_thickness(5.0)
 	mouse_entered.connect(_on_mouse_entered)
@@ -11,7 +12,11 @@ func _ready() -> void:
 func set_outline_thickness(new_thickness: float) -> void:
 	if canvas_group_has_material:
 		canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
+
 func _on_mouse_entered() -> void:
 	set_outline_thickness(10.0)
+
+
 func _on_mouse_exited() -> void:
 	set_outline_thickness(5.0)
--- M06.looting_workbook/practices/L3.P2.sting_the_balloon/balloon_sting.gd
+++ M06.looting_workbook/practices/L3.P2.sting_the_balloon/balloon_sting.gd
@@ -4,5 +4,6 @@ extends Area2D
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 
+
 func _on_mouse_entered() -> void:
 	queue_free()
