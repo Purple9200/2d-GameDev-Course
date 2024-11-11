64391c816b6c644111a021c41f3342628321d037
Purple9200 <159182392+Purple9200@users.noreply.github.com>
2024-11-11 01:25:09 -0800

    M7 L8

--- M07.telling_a_story_workbook/expressions.gd
+++ M07.telling_a_story_workbook/expressions.gd
@@ -1,24 +1,20 @@
 extends Control
-
 var bodies := {
 	"sophia": preload("res://assets/sophia.png"),
 	"pink": preload("res://assets/pink.png")
 }
-
 var expressions := {
 	"happy": preload("res://assets/emotion_happy.png"),
 	"regular": preload("res://assets/emotion_regular.png"),
 	"sad": preload("res://assets/emotion_sad.png"),
 }
-
-@onready var body: TextureRect = %Body
-@onready var expression: TextureRect = %Expression
-@onready var button_sophia: Button = %ButtonSophia
-@onready var button_pink: Button = %ButtonPink
-@onready var button_regular: Button = %ButtonRegular
-@onready var button_sad: Button = %ButtonSad
-@onready var button_happy: Button = %ButtonHappy
-
+@onready var body = %Body
+@onready var expression = %Expression
+@onready var button_sophia = %ButtonSophia
+@onready var button_pink = %ButtonPink
+@onready var button_regular = %ButtonRegular
+@onready var button_sad = %ButtonSad
+@onready var button_happy = %ButtonHappy
 
 func _ready() -> void:
 	body.texture = bodies["pink"]
@@ -33,10 +29,9 @@ func _ready() -> void:
 	button_regular.pressed.connect(func() -> void:
 		expression.texture = expressions["regular"]
 	)
-	button_happy.pressed.connect(func() -> void:
-		expression.texture = expressions["happy"]
-	)
 	button_sad.pressed.connect(func() -> void:
 		expression.texture = expressions["sad"]
 	)
-	
+	button_happy.pressed.connect(func() -> void:
+		expression.texture = expressions["happy"]
+	)
